22a83c2eb59171051cefd7fc0d41880d465032ec
kor216465 <kor216465@gmail.com>
2019-07-01 01:02:17 +0900

    added custom collectionView cell

---
+++ JeffMall/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  JeffMall
+//
+//  Created by Jeff Jeong on 2019. 7. 1..
+//  Copyright © 2019년 Tuentuenna. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+ 
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
